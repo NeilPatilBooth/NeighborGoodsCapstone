022693c81dd7b412ba0d833875a2a25395c3256b
NeilPatilBooth <npatil@ChicagoBooth.edu>
2024-12-11 21:56:50 +0000

    added user accounts, sign in nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   
   # Route for homepage will be furniture index page 
   root to: "furnitures#index"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_10_211449) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_11_205042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
